80638fdb03f05c1cf2396fede0a0e32acdeded9c
Jonathan <115780790+RaffetsederJonathan@users.noreply.github.com>
2026-07-29 20:57:38 +0200

    uolzgkuzghbu

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "BoniJoniProduction-GeoManhunt-0.0.22";
+const cacheName = "BoniJoniProduction-GeoManhunt-0.0.28";
 const contentToCache = [
     "Build/WebGL-Manhunt.loader.js",
     "Build/WebGL-Manhunt.framework.js",
