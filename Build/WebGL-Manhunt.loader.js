db63557dc08be6e76c6c4c093417a02f57263be9
Jonathan <115780790+RaffetsederJonathan@users.noreply.github.com>
2026-07-29 18:00:02 +0200

    fixed map?

--- Build/WebGL-Manhunt.loader.js
+++ Build/WebGL-Manhunt.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 80482339,
+    wasmFileSize: 80483049,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
